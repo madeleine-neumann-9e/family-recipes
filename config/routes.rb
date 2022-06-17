4beaa4bc76f87e2d3ea190cfeba8a455de16509b
Madeleine Neumann <madeleine.neumann@9elements.com>
2022-06-17 22:37:56 +0200

    adds views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'recipe/index'
+  get 'recipe/new'
+  get 'recipe/create'
+  get 'recipe/edit'
+  get 'recipe/update'
   root to: 'landingpage#index'
 
   namespace :backyard do
     root to: 'recipes#index'
+    resources :ingredients
+    resources :recipes
   end
 end
