10fbe218a155c5a5f167d16859439aa73ed31e03
Madeleine Neumann <madeleine.neumann@9elements.com>
2022-06-17 23:02:16 +0200

    makes ingredients createable and updateable

--- app/controllers/backyard/ingredients_controller.rb
+++ app/controllers/backyard/ingredients_controller.rb
@@ -1,5 +1,45 @@
 module Backyard
   class IngredientsController < ApplicationController
+    def index
+      @ingredients = Ingredient.all
+    end
 
+    def new
+      @ingredient = Ingredient.new
+      @url = backyard_ingredients_path
+    end
+
+    def create
+      @ingredient = Ingredient.new(ingredient_params)
+      if @ingredient.save
+        redirect_to backyard_ingredients_path
+      else
+        render :new, flash.alert("something went wrong")
+      end
+    end
+
+    def edit
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    def update
+      @ingredient = Ingredient.find(params[:id])
+      @url = backyard_ingredient_path
+      if @ingredient.update(ingredient_params)
+        redirect_to backyard_ingredients_path
+      else
+        render :new, flash.alert("something went wrong")
+      end
+    end
+
+    private
+    def ingredient_params
+      params.require(:ingredient).permit(
+        :name_singular_de,
+        :name_singular_en,
+        :name_plural_de,
+        :name_plural_en
+      )
+    end
   end
 end
